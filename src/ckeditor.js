85db763dc9b21e5d884888cd55446f9a92c25077
unknown <jonesaa5@msu.edu>
2020-05-15 21:11:24 -0400

    Initial CKEditor5 Custom Balloon Block Build

---
+++ src/ckeditor.js
@@ -0,0 +1,115 @@
+/**
+ * @license Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+// The editor creator to use.
+import BalloonEditorBase from '@ckeditor/ckeditor5-editor-balloon/src/ballooneditor';
+
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
+import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
+import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
+import BlockToolbar from '@ckeditor/ckeditor5-ui/src/toolbar/block/blocktoolbar';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
+import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading';
+import Image from '@ckeditor/ckeditor5-image/src/image';
+import ImageTextAlternative from '@ckeditor/ckeditor5-image/src/imagetextalternative';
+import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
+import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
+import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
+import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
+import Indent from '@ckeditor/ckeditor5-indent/src/indent';
+import Link from '@ckeditor/ckeditor5-link/src/link';
+import List from '@ckeditor/ckeditor5-list/src/list';
+import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
+import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
+import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize';
+import Title from '@ckeditor/ckeditor5-heading/src/title';
+import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline';
+
+import '../theme/theme.css';
+import '../theme/custom.css';
+
+export default class BalloonEditor extends BalloonEditorBase {}
+
+// Plugins to include in the build.
+BalloonEditor.builtinPlugins = [
+	Essentials,
+	BlockToolbar,
+	Title,
+	Bold,
+	Italic,
+	Link,
+	Image,
+	ImageToolbar,
+	ImageCaption,
+	ImageStyle,
+	ImageTextAlternative,
+	ImageUpload,
+	ImageResize,
+	MediaEmbed,
+	HorizontalLine,
+	BlockQuote,
+	Heading,
+];
+
+// Editor configuration.
+BalloonEditor.defaultConfig = {
+	blockToolbar: [
+		'imageUpload',
+		'mediaEmbed',
+		'|',
+		'blockQuote',
+		'horizontalLine',
+		'|',
+		'undo',
+		'redo'
+	],
+	toolbar: {
+		items: [
+			'bold',
+			'italic',
+			'link',
+			'|',
+			'heading',
+		]
+	},
+	image: {
+		toolbar: [
+			'imageStyle:full',
+			'|',
+			'imageStyle:alignLeft',
+			'imageStyle:alignCenter',
+			'imageStyle:alignRight',
+			'|',
+			'imageTextAlternative'
+		],
+		styles: [
+			'full',
+			'alignLeft',
+			'alignCenter',
+			'alignRight',
+		]
+	},
+	heading: {
+		options: [
+			{ model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+			{ model: 'subtitle',
+				view: { name: 'h2', classes: 'subtitle' },
+				title: 'Subtitle',
+				class: 'ck-heading_heading2_subtitle',
+				// It needs to be converted before the standard 'heading2'.
+				converterPriority: 'high'
+			},
+			{ model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
+			{ model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
+		]
+	},
+	// This value must be kept in sync with the language defined in webpack.config.js.
+	language: 'en'
+};
